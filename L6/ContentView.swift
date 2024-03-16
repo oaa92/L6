b2dadce881b85c85a7cf30e4e56c37275a6769b6
Anatoliy Odinetskiy <aodinetsky@yandex-team.ru>
2024-03-17 00:11:56 +0700

    2

--- L6/ContentView.swift
+++ L6/ContentView.swift
@@ -8,61 +8,69 @@ enum Orientation {
 
 struct ContentView: View {
 
-    let count: Int = 7
+    static var count: Int = 7
+
     @State var orientation: Orientation = .horizontal
+    @State var rectSize: CGFloat = 0
+    @State var offsetsX: [CGFloat] = Array(repeating: 0, count: ContentView.count)
+    @State var offsetsY: [CGFloat] = Array(repeating: 0, count: ContentView.count)
+    @State var size: CGSize = .zero
 
     var body: some View {
         GeometryReader { geometry in
-            let size = geometry.size
-            ForEach(0..<count, id: \.self) { index in
-                let x: CGFloat
-                let y: CGFloat
-                let rectSize: CGFloat
-                switch orientation {
-                case .horizontal:
-                    let index = CGFloat(index)
-                    let spacing: CGFloat = 8
-                    let count = CGFloat(count)
-                    rectSize = (size.width - (spacing * (count - 1))) / count
-                    let offsetY = size.height / 2
-                    x = rectSize * index + spacing * index
-                    y = offsetY
-                case .diagonal:
-                    let index = CGFloat(index)
-                    let count = CGFloat(count)
-                    rectSize = size.height / count
-                    let diffX = (size.height - size.width) / (count - 1)
-                    let offsetX = size.height / count
-                    x = offsetX * (count - index - 1) - diffX * (count - index - 1)
-                    y = rectSize * index
-                }
-
-                let rectSize = makeRectSize()
-                RoundedRectangle(cornerSize: CGSize(width: rectSize/4, height: rectSize/4))
+            ForEach(0..<ContentView.count, id: \.self) { index in
+                RoundedRectangle(cornerSize: CGSize(width: rectSize/8, height: rectSize/8))
                     .foregroundStyle(Color.blue)
                     .frame(width: rectSize, height: rectSize)
-                    .offset(x: x, y: y)
+                    .offset(x: offsetsX[index], y: offsetsY[index])
             }
-
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    size = geometry.size
+                    update(orientation: orientation)
+                }
+            })
         }
-        .background(Color.gray)
+        .onTapGesture {
+            let orientation: Orientation
+            if self.orientation == .horizontal {
+                orientation = .diagonal
+            }
+            else {
+                orientation = .horizontal
+            }
+            withAnimation {
+                update(orientation: orientation)
+            }
+        }
     }
 
-    func makeRectSize(orientation: Orientation, size: CGSize) -> CGFloat {
-        let count = CGFloat(count)
+    func update(orientation: Orientation) {
+        self.orientation = orientation
+        rectSize = makeRectSize(orientation: orientation)
+        print(rectSize)
+        for index in 0..<ContentView.count {
+            let (x, y) = makeOffset(orientation: orientation, index: index)
+            offsetsX[index] = x
+            offsetsY[index] = y
+        }
+    }
+
+    func makeRectSize(orientation: Orientation) -> CGFloat {
+        let count = CGFloat(ContentView.count)
         switch orientation {
         case .horizontal:
             let spacing: CGFloat = 8
             return (size.width - (spacing * (count - 1))) / count
-        case . diagonal:
+        case .diagonal:
             return size.height / count
         }
     }
 
-    func makeOffset(orientation: Orientation, size: CGSize, index: Int) -> (CGFloat, CGFloat) {
+    func makeOffset(orientation: Orientation, index: Int) -> (CGFloat, CGFloat) {
         let index = CGFloat(index)
-        let count = CGFloat(count)
-        let rectSize = makeRectSize(orientation: orientation, size: size)
+        let count = CGFloat(ContentView.count)
+        let rectSize = makeRectSize(orientation: orientation)
         switch orientation {
         case .horizontal:
             let spacing: CGFloat = 8
